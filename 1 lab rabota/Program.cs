a39e0d7a17de59b957ae37b2d77c65e5dbd2bb2b
annabazenovanka <annabazenovanka@gmail.com>
2019-04-08 17:23:11 +0300

    Добавьте файлы проекта.

---
+++ 1 lab rabota/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            for (; ; )
+            {
+                bool continu = true;
+
+
+                Console.Write("Enter first number and press enter: ");
+
+                try
+                {
+                    a = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error");
+                    continu = false;
+                }
+                if (continu == true)
+                    break;
+            }
+            ///////////////
+            for (; ; )
+            {
+                bool continu = true;
+
+                Console.Write("Enter second number and press enter: ");
+
+                try
+                {
+                    b = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error");
+
+                    continu = false;
+                }
+                if (continu == true)
+                    break;
+                ///////////
+            }
+            Console.Write("result=" + Sum(a, b));
+            Console.ReadLine();
+        }
+        public static int Sum(int a, int b)
+        {
+            return (a + b);
+        }
+
+    }
+}
